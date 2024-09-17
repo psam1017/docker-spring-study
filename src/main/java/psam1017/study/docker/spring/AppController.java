50178122d3db9c634782a55103c28bcf93985ff4
sam_nodeblack <psam1017@nodeblack.io>
2024-09-17 15:29:39 +0900

    image scaling

--- src/main/java/psam1017/study/docker/spring/AppController.java
+++ src/main/java/psam1017/study/docker/spring/AppController.java
@@ -1,21 +1,29 @@
 package psam1017.study.docker.spring;
 
-import org.springframework.data.redis.core.RedisTemplate;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class AppController {
 
-    private final RedisTemplate<String, Object> redisTemplate;
-
-    public AppController(RedisTemplate<String, Object> redisTemplate) {
-        this.redisTemplate = redisTemplate;
-    }
-
     @GetMapping("")
-    public String hello() {
-        redisTemplate.opsForValue().set("key", "Hello, Docker!");
-        return "Hello, Docker!";
+    public Map<String, String> getInfo(HttpServletRequest request) {
+        Map<String, String> response = new HashMap<>();
+
+        if (request.getHeader("X-Real-IP") != null) {
+            response.put("X-Real-IP", request.getHeader("X-Real-IP"));
+        }
+        if (request.getHeader("X-Forwarded-For") != null) {
+            response.put("X-Forwarded-For", request.getHeader("X-Forwarded-For"));
+        }
+        if (request.getHeader("Host") != null) {
+            response.put("Host", request.getHeader("Host"));
+        }
+
+        return response;
     }
 }
